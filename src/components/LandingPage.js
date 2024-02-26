a3e5b83d64802384925cd4bc92f46e9cf8a3fbf6
cess <pmariate@up.edu.ph>
2024-02-26 23:08:41 +0800

    ui : edited the layout and design of page

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,25 +1,29 @@
 import React from 'react';
 import '../App.css'; // Import CSS file
-import { FaSearch } from 'react-icons/fa';
 
 const LandingPage = () => {
   return (
     <div className="container">
       <nav>
-        <ul class = "navbar-list">
+        <ul className="navbar-list">
           <li><a href="#about">About Us</a></li>
           <li><a href="#gallery">Gallery</a></li>
           <li><a href="#contact">Contact</a></li>
-          <FaSearch />
         </ul>
       </nav>
-      <header>
-        <h1 class = "welcome-section">Welcome to Our Landing Page!</h1>
-      </header>
 
- 
+      <section id="welcome-section" className="section welcome-section">
+        <div className="container">
+          <div className="row">
+            <div className="header-text">
+              <h2 style={{ color: 'white', fontSize: '55px', marginBottom : '12px' }}>Get out and explore</h2>
+              <h2 style={{ color: '#ADDFAD', fontSize: '22px', fontFamily: 'Helvetica' }}>Lorem ipsum dolor sit amet. At doloribus deleniti sed facere nostrum quo nulla pariatur ut voluptatibus amet ut galisum dolores non dignissimos reiciendis qui minus obcaecati.</h2>
+            </div>
+          </div>
+        </div>
+      </section>
 
-
+      {/* Other sections */}
       <section id="about">
         <h2>About Us</h2>
         <p>This is where you can describe your company or product.</p>
